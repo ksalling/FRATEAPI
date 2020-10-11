f4ce4ab47c570e1ef099b1b6e393b0a4a766bfa3
ksalling <ksalling@gmail.com>
2020-10-11 01:49:27 -0700

    app

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,30 @@
 import App from 'next/app';
 import Head from 'next/head';
-import { AppProvider } from '@shopify/polaris';
-import { Provider } from '@shopify/app-bridge-react';
-import '@shopify/polaris/styles.css';
-import translations from '@shopify/polaris/locales/en.json';
-import Cookies from 'js-cookie';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+//import { AppProvider } from '@shopify/polaris';
+//import { Provider } from '@shopify/app-bridge-react';
+//import '@shopify/polaris/styles.css';
+//import translations from '@shopify/polaris/locales/en.json';
+//import Cookies from 'js-cookie';
+//import ApolloClient from 'apollo-boost';
+//import { ApolloProvider } from 'react-apollo';
 
-//import Index from './index';
+import Index from './index';
 
 //import CssBaseline from '@material-ui/core/CssBaseline';
-import 'fontsource-roboto';
+//import 'fontsource-roboto';
 
-const client = new ApolloClient({
-    fetchOptions: {
-        credentials: 'include'
-    }
-})
+// const client = new ApolloClient({
+//     fetchOptions: {
+//         credentials: 'include'
+//     }
+// })
 
 
 class MyApp extends App {  
 
   render() {
-    const { Component, pageProps } = this.props;
-    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+  //  const { Component, pageProps } = this.props;
+  //  const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
     console.log('test')
 
 
@@ -35,7 +35,7 @@ class MyApp extends App {
           <title>Frate</title>
           <meta charSet="utf-8" />
         </Head>
-        <Component {...pageProps} />
+        <Index />
       </React.Fragment>
     );
   }
