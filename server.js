da3cbe0029efb76fcbae27b5a84b5b69ebf503ac
ksalling <ksalling@gmail.com>
2020-10-11 01:29:48 -0700

    ctx

--- server.js
+++ server.js
@@ -866,7 +866,7 @@ app.prepare().then(() => {
     await hookHandle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
-
+    ctx.redirect('/');
   });
 
   // server.listen(port, () => {
